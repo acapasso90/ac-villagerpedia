c7e8fec1557c4c2c796bea6ba46cb54ede6378b8
acapasso90 <69538713+acapasso90@users.noreply.github.com>
2021-11-09 18:22:59 -0500

    moved footer to bottom of page, made mobile sized exit button smaller
    moved footer to bottom of page, made mobile sized exit button smaller

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import VillagerSearch from './VillagerSearch';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <VillagerSearch />
       <footer>
         <p><div style={{fontSize: "10pt"}}>All images, information, quotes, and characters found on the app are the sole property of Nintendo and Animal Crossing and are only used for non-commercial and educational purpose. </div>
         App made by <a href="https://www.amandacapasso.com/" rel="noreferrer" target="_blank">Amanda Capasso</a> </p></footer>
+    </div>
     </div>
   );
 }
